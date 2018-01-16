0f3ab6b18d7adc9a4ad9656a4821f549c7f1a1bd
Giovani Gonzalez <darkgeat@gmail.com>
2018-01-16 09:42:31 -0600

    implemntation of the styles and initial approach to UI

--- app/src/main/java/training/giovas/sunshine/MainActivity.kt
+++ app/src/main/java/training/giovas/sunshine/MainActivity.kt
@@ -2,11 +2,16 @@ package training.giovas.sunshine
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        setTitle("Prueba de titulo Giovas")
     }
 }
